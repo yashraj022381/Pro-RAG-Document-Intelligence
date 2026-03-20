300e08e0177a9faa5129e51941486c4061aef3e1
Yashraj Jagdale <159432802+yashraj022381@users.noreply.github.com>
2026-03-20 19:47:35 +0530

    Refactor app.py for multi-file processing and chat

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
-from langchain_community.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
 
 
